feed9a7d898b54027a866e3acaa017f2f921702e
Administrator <ronghaoli@yeah.net>
2018-03-16 16:38:07 +0800

    user pojo

---
+++ src/com/example/pojo/User.java
@@ -0,0 +1,15 @@
+package com.example.pojo;
+
+public class User {
+
+    private String name;
+    private int age;
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+}
